cbf1d52b2ca500226dcf83c5026162bb69ad4041
golangify <dimadrajv062@gmail.com>
2024-12-29 17:56:08 +0300

    add some statistics to /start text

--- handlers/command/start.go
+++ handlers/command/start.go
@@ -9,11 +9,19 @@ import (
 )
 
 func (h *CommandHandler) Start(update *tgbotapi.Update, user *models.User, args []string) {
+	var totalUsersCount, totalTrackedTopicsCount int64
+	h.Database.Model(&models.User{}).Count(&totalUsersCount)
+	h.Database.Model(&models.TrackedTopic{}).Count(&totalTrackedTopicsCount)
+
 	msg := tgbotapi.NewMessage(update.FromChat().ID, fmt.Sprintf(
 		"<b>Привет, %s</b>!\n\n"+
 			"В этом боте можно отслеживать все новые комментарии под темами с сайта shikimori.one.\n\n"+
-			"Сводка команд - /help",
+			"Сводка команд - /help\n\n"+
+			"Всего пользователей: %d\n"+
+			"Всего отслеживаемых топиков: %d",
 		html.EscapeString(update.SentFrom().FirstName),
+		totalUsersCount,
+		totalTrackedTopicsCount,
 	))
 	msg.ParseMode = tgbotapi.ModeHTML
 	h.Bot.Send(msg)
