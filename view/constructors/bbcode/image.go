12f3f50549d252719fed576f09ff7ad127fbc041
golangify <dimadrajv062@gmail.com>
2025-02-19 22:48:15 +0300

    remove space from image command

--- view/constructors/bbcode/image.go
+++ view/constructors/bbcode/image.go
@@ -10,7 +10,7 @@ var bbImageRegexp = regexp.MustCompile(`\[(?:image|poster)=(\d+)[^\]]*\]`)
 
 func (p *BBCodeParser) parseImage(text string) string {
 	for _, match := range bbImageRegexp.FindAllStringSubmatch(text, -1) {
-		text = strings.ReplaceAll(text, match[0], fmt.Sprint(" /image ", match[1]))
+		text = strings.ReplaceAll(text, match[0], fmt.Sprint(" /image", match[1], " "))
 	}
 	return text
 }
